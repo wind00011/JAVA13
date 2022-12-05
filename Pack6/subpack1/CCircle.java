7198eff6d4a2ba8d8f51d471719f8cbb0b836e37
wind00011 <113489040+wind00011@users.noreply.github.com>
2022-12-06 02:31:28 +0800

    Delete Pack6 directory

--- Pack6/subpack1/CCircle.java
+++
@@ -1,7 +0,0 @@
-package Pack6.subpack1;
-
-public class CCircle {
-	public void show() {
-		System.out.println("show() method of class CCircle called");
-	}
-}
--- Pack6/subpack2/CRectangle.java
+++
@@ -1,7 +0,0 @@
-package Pack6.subpack2;
-
-public class CRectangle {
-	public void show() {
-		System.out.println("show() method of class CRectangle called");
-	}
-}
